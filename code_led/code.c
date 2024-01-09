54490eb0e2139061ba36c9bb21ec9b2c75998dde
Shanmugapriyamv <155048322+Shanmugapriyamv@users.noreply.github.com>
2024-01-09 15:28:17 +0530

    s

--- code_led/code.c
+++ code_led/code.c
@@ -255,7 +255,7 @@ void main()
 P45=1;P44=1;DelayXms(2000);P45=0;P44=0;DelayXms(2000);
 //P11=0;  // TO CHECK		
 				P33=0; // .............. made changes now  3/1
-			
+			//Newly updated 
 			
     }
 }
